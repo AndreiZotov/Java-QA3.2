7ca2a2ec64590498eb33d469fa627f89c4a131ed
Andrei Zotov <777zotov@gmail.com>
2023-04-03 19:19:47 +0300

    Second commit

--- src/bmiService.java
+++ src/bmiService.java
@@ -2,7 +2,7 @@ public class bmiService {
 
     public int calculate(int weightInKg, double heightInMeters) {
 
-       double bmiIndex = weightInKg / heightInMeters / heightInMeters;
+        double bmiIndex = weightInKg / heightInMeters / heightInMeters;
 
         return (int) bmiIndex;
     }
